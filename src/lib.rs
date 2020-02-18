ed19cdcc82b6e0c8b96703ccf6def0a4dc59f8c3
Arthur Goldman <arthur@goldman-tribe.org>
2020-02-18 09:43:19 -0500

    version bump; change my name; remove outdated features

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,7 @@
 #![cfg_attr(feature = "no_std",  no_std)]
-#![feature(try_from, const_fn)]
 
 pub mod type_tags;
 pub mod bit_utils;
 pub mod pointer_tags;
 
 pub use crate::type_tags::{TypeId, ThinTypeId, TypeError, TypeIdTooLargeError};
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
